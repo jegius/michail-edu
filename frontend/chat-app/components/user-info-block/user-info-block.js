fac53c501463a821753a48229c9f4a4db1dd8dda
J-Lany <ddzheylani@gmail.com>
2024-02-29 12:49:22 +0300

    Remove default props

--- frontend/chat-app/components/user-info-block/user-info-block.js
+++ frontend/chat-app/components/user-info-block/user-info-block.js
@@ -1,5 +1,7 @@
 import { createUserInfoBlockTemplate } from "./user-info-block.template";
 
+const defaultAvatar = 'https://img.freepik.com/free-vector/hand-drawn-caricature-illustration_23-2149760515.jpg?t=st=1709056676~exp=1709060276~hmac=55f551634adbc78dfe62163921b8fd5ae89b2d05a7ef69f29f5cb3043d09ef1d&w=1380'
+
 export class UserInfoBlock extends HTMLElement {
   static get name() {
     return "user-info-block";
@@ -13,7 +15,7 @@ export class UserInfoBlock extends HTMLElement {
 
   render() {
     const templateElm = document.createElement("template");
-    templateElm.innerHTML = createUserInfoBlockTemplate({});
+    templateElm.innerHTML = createUserInfoBlockTemplate({name: 'Olga', avatar: defaultAvatar, status: 'Active'});
     this.shadowRoot.appendChild(templateElm.content.cloneNode(true));
   }
 }
--- frontend/chat-app/components/user-info-block/user-info-block.template.js
+++ frontend/chat-app/components/user-info-block/user-info-block.template.js
@@ -1,9 +1,13 @@
 import { getUserInfoBlockComponentStyle } from "./user-info-block.styles"
 
-const defaultAvatar = 'https://img.freepik.com/free-vector/hand-drawn-caricature-illustration_23-2149760515.jpg?t=st=1709056676~exp=1709060276~hmac=55f551634adbc78dfe62163921b8fd5ae89b2d05a7ef69f29f5cb3043d09ef1d&w=1380'
 
 
-export function createUserInfoBlockTemplate ({name = 'Olga', avatar = defaultAvatar, status = 'Active'}) {
+
+export function createUserInfoBlockTemplate (props) {
+  if (!props) {
+    return null
+  }
+  const {name, avatar, status} = props;
   return (`
   ${getUserInfoBlockComponentStyle()}
   <div class="user-info">
